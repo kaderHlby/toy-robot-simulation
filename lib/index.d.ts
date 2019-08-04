5a3fb228cf0c923e2e8901eb02a7b1913b1e0a94
abdulkader <kader@accendo.com.my>
2019-08-04 20:43:56 +0800

    accept file path as an argument and validate file dose exist

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -4,3 +4,7 @@ declare const clear: any;
 declare const figlet: any;
 declare const path: any;
 declare const program: any;
+declare var fs: any;
+declare function fileDoesNotExist(path: any): boolean;
+declare function parseFile(path: any): any;
+declare function run(path: any): void;
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,42 @@
 #!/usr/bin/env node
-const chalk = require('chalk');
-const clear = require('clear');
-const figlet = require('figlet');
-const path = require('path');
-const program = require('commander');
+const chalk = require("chalk");
+const clear = require("clear");
+const figlet = require("figlet");
+const path = require("path");
+const program = require("commander");
+var fs = require("fs");
 
 clear();
 console.log(
-  chalk.red(
-    figlet.textSync('robot-cli', { horizontalLayout: 'full' })
-  )
+  chalk.red(figlet.textSync("robot-cli", { horizontalLayout: "full" }))
 );
 
+function fileDoesNotExist(path: any) {
+  try {
+    if (fs.existsSync(path)) {
+      return false;
+    }
+  } catch (err) {}
+  return true;
+}
+
+function parseFile(path: any) {
+  return fs
+    .readFileSync(path)
+    .toString()
+    .split("\n");
+}
+
+function run(path: any) {
+  if (fileDoesNotExist(path)) {
+    console.error(`no such file or directory, ${path}`);
+    return;
+  }
+  const commands = parseFile(path);
+  console.log(commands);
+}
+
 program
-  .version('0.0.2')
-  .description("A CLI for simulating robot's movement")
-  .option('-f, --file', 'Input from file')
-  .option('-i, --standard-input', 'Input from standard input')
+  .version("0.0.1")
+  .option("-p, --path <path>", "commands file path", run) //todo make path required
   .parse(process.argv);
